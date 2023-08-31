3bad80c8606cc9f17722e5d6305f66fd3650f424
caglatunc <caglatunc03@gmail.com>
2023-09-01 02:20:49 +0300

    .

--- JS/brick-game/script.js
+++ JS/brick-game/script.js
@@ -1,22 +1,28 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+let interval;
 const height = canvas.height;
 const width = canvas.width;
-
+const x = width/2;
+const y = height-30;
 //arrow function: method oluşturma yöntemi
 const oyunuCiz = () => {
     tahtayiTemizle();
+    topuCiz();
 
-    ctx.beginPath();
-        ctx.arc(width/2, height-30, 10,0,Math.PI*2);
-        ctx.fillStyle = "#0095DD"
-        ctx.fill();
-    ctx.closePath();
 }
 
 const tahtayiTemizle = () =>{
     ctx.clearRect(0,0, width,height); //Önce tabloyu sıfırlıyoruz. 
 }
 
-oyunuCiz();
+const topuCiz = () =>{
+    ctx.beginPath();
+    ctx.arc(x, y, 10,0,Math.PI*2); //Şeklini belirtiyorum
+    ctx.fillStyle = "#0095DD" // rengini belirtiyorum
+    ctx.fill(); // fill ile birlikte şeklini ve rengini uygulatıyorum.
+    ctx.closePath();
+}
+
+interval =setInterval(oyunuCiz,10);
