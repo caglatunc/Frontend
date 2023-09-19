0be9e7d74b2d57928420312ee8781338d790aab3
caglatunc <caglatunc03@gmail.com>
2023-09-20 02:12:54 +0300

    .

---
+++ HTML/CvProject/client/script.js
@@ -0,0 +1,60 @@
+getMyInformation();
+
+function getMyInformation(){
+    axios.get("http://localhost:5000/api/getMyInformation")
+    .then(res=>{
+        const person = res.data;
+        document.getElementById("name").innerText=person.name;
+        document.getElementById("title").innerText=person.title;
+        document.getElementById("avatar").src=person.avatar;
+        document.getElementById("phone").innerText=person.phone;
+        document.getElementById("email").innerText=person.email;
+        document.getElementById("website").innerText=person.website;
+        document.getElementById("socialMedia").innerText=person.socialMedia;
+        document.getElementById("address").innerText=person.address;
+        document.getElementById("aboutMe").innerHTML=person.aboutMe;
+        console.log(person);
+    });
+
+getMySkills();
+}
+function getMySkills(){
+    axios.get("http://localhost:5000/api/getMySkills")
+    .then(res=> {
+        const skills = res.data;
+        let text = "";
+        for(let skill of skills){
+            text += `
+            <li>
+            <span class="percent">
+            <p>${skills.title}</p>
+                <div style="width:${skill.percent}%; background-color:black "></div>
+            </span>
+        </li>`
+        }
+        document.getElementById("skills").innerHTML= text;
+    })
+    
+}
+
+
+
+
+// let skillElementId = 1;
+// function addSkill(){
+//     skillElementId++;
+//     const value = `<li id="liSkill-${skillElementId}">
+//     <span class="text">HTML
+//     <button onclick="removeSkill('liSkill-${skillElementId}')">-</button>
+//     </span>
+//     <span class="percent">
+//     <div style="width: 90%;"></div>
+//     </span>
+//     </li>`
+
+//     document.getElementById("skills").innerHTML += value;
+// }
+
+// function removeSkill(id){ //Hangi elementi sileceksem onun id'sini ister.
+//     document.getElementById(id).remove();
+// }
---
+++ HTML/CvProject/server/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+
+const person = {
+    name:"Ercan Savaş",
+    title:"Software Developer",
+    phone:"+90 500 000 00 00",
+    email:"ercan@gmail.com",
+    website:"www.ercan.com",
+    socialMedia: "www.linkedin.com/me",
+    address:"İstanbul / Turkey",
+    avatar:"img.jpg",
+    aboutMe:` 
+    <div class="show-more-module--container--2QPRN"><span id="u146-show-more--1" data-type="checkbox" data-checked="checked" style="display:none"></span><div class="show-more-module--content--cjTh0 show-more-module--with-gradient--1ZDrA" style="max-height:32rem"><div tabindex="0"><div data-purpose="instructor-description"><p>Hello! I'm <strong>Çağla Tunç Savaş</strong>.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit magnam nisi dolores repellendus
+    corporis distinctio cupiditate, saepe eius</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit magnam nisi dolores repellendus
+    corporis distinctio cupiditate, saepe eius<strong>Udemy </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit magnam nisi dolores repellendus
+    corporis distinctio pcupiditate, saepe eius.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit magnam nisi dolores repellendus
+    corporis distinctio cupiditate, saepe eius</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit magnam nisi dolores repellendus
+    corporis distinctio cupiditate, saepe eius</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit magnam nisi dolores repellendus
+    corporis distinctio cupiditate, saepe eius</p></div></div></div></div>
+    `
+}
+
+let skills =[
+    {
+        title:"C#",
+        percent:80
+    },
+    {
+        title:"HTML",
+        percent:100
+    },
+    {
+        title:"JS",
+        percent:30
+    }
+]
+
+//kontrol amaçlı api isteği
+app.get("", (req, res) =>{
+    res.json({message:"Api çalışıyor."})
+})
+
+app.get("/api/getMyInformation", (req,res)=> {
+    res.json(person);
+})
+
+
+app.get("/api/getMySkills", (req,res)=> {
+    res.json(skills);
+})
+
+
+
+app.listen(5000, ()=> console.log("Uygulama http://localhost:5000 üzerinden ayakta")); //uygulamayı çalıştığım zaman bu porttan ayağa kalkıyor
